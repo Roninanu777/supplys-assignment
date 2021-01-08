639c4145842d415ee5f43f1fec174e37ead37bb9
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2021-01-08 13:54:52 +0530

    entry file

---
+++ task2/src/App.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Cart from "./components/Cart";
+import Dropdown from "./components/Dropdown";
+
+function App() {
+    const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [carts, setCarts] = useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json);
+                setLoading(false);
+                setImages((prev) => [...prev, ...json]);
+            });
+
+        fetch("https://fakestoreapi.com/carts?limit=3")
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json);
+                setCarts([...json]);
+            });
+    }, []);
+
+    return (
+        <div className="container my-5">
+            <Dropdown />
+            <Switch>
+                <Route
+                    path="/cart/:id"
+                    children={
+                        <Cart images={images} carts={carts} loading={loading} />
+                    }
+                ></Route>
+                <Redirect to="/cart/1" />
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
